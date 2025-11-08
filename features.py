3b4e677edfaef4344eaee0f4d26028ffa482640f
KOLLGO <134609826+KOLLGO@users.noreply.github.com>
2025-11-08 13:30:13 +0100

    Implement Mentions Logic, closes #24

--- features.py
+++ features.py
@@ -1,10 +1,12 @@
 # Feature Extraction
 import pandas as pd
+import re
 
 # --------Pseudo DataFrame---------
 df = pd.read_csv(
     "tar.csv", sep=";", nrows=50, on_bad_lines="skip", na_filter=True
 )  # first 50 rows
+df["description"] = df["description"].str.lower()  # lowercase descriptions
 print(df.head())
 
 
@@ -88,8 +90,29 @@ def extract_mentions(df):
     in: df
     current out: df with pronouns features
     desired out: ???
+    todo: maybe other mentions depending on final dataset
     """
-    pass
+    mentions_list = ["ind", "pre", "pol", "grp"]  # list of mentions
+    df_mentions = pd.DataFrame()
+    df_mentions["id"] = 0  # id column
+    # all mentions columns
+    for mention in mentions_list:
+        df_mentions[f"mention_{mention}"] = 0
+
+    # loop through rows
+    for idx, row in df.iterrows():
+        id = row["id"]  # get id
+        text = row["description"]  # get description
+        print(text)
+
+        # loop through mentions
+        for mention in mentions_list:
+            column_name = f"mention_{mention}"  # set column name
+            count = len(re.findall(rf"@{mention}\b", text))  # count occurrences
+            df_mentions.loc[idx, "id"] = id  # place id in mention df
+            df_mentions.loc[idx, column_name] = count  # enter counts
+
+    return df_mentions
 
 
 def extract_word_n_grams(df):
@@ -102,3 +125,4 @@ def extract_word_n_grams(df):
 
 print(extract_pronouns(df))
 print(extract_generics(df))
+print(extract_mentions(df))
