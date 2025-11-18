29a04804a12b1c9b27f0873ceeeb9197bb9334c0
Vanessa <vanessa.greiner2004@gmail.com>
2025-11-18 12:29:57 +0100

    Kommentierung

--- classify.py
+++ classify.py
@@ -10,44 +10,41 @@ import joblib
 
 # ------------------ Daten vorbereiten ------------------
 
-X_train, y_train, X_test, y_test = get_feature_matrices()
+X_train, y_train, X_test, y_test = get_feature_matrices() # Uebergabe Feature-Matrix
 
 # ------------------ Pipeline erstellen ------------------
 
 pipeline = Pipeline([
-    ("scaler", StandardScaler()),
-    ("svc", SVC(kernel="rbf", probability=True))
+    ("scaler", StandardScaler()),   # standartisiert Werte
+    ("svc", SVC(kernel="rbf"))    # SVC = Support Vector Classifyer, radial basis function Kernel
 ])
 
 # ------------------ Parameteroptimierung ------------------
 
 param_grid = {
-    'svc__C': [0.1, 1, 10],
-    'svc__gamma': [0.001, 0.01, 0.1, 'scale']  # 'scale' ist ein automatischer Standardwert von 0,22
+    'svc__C': [0.1, 1, 10],     # gewaehlte Werte fuer Parameter C (Bestrafung Fehlklassifikation)
+    'svc__gamma': [0.001, 0.01, 0.1, 'scale']
+    # gewaehlte Werte fuer Parameter Gamma (Einfluss einzelnes Trainingsbeispiel)
+    # 'scale' ist ein automatischer Standardwert von 0,22
 }
 
 grid_search = GridSearchCV(
-    estimator=pipeline,
-    param_grid=param_grid,
-    cv=5,
-    scoring='accuracy',
-    verbose=1
+    estimator=pipeline,  # zu bewertende Modell -> Pipeline (Standardsclaer + SVC)
+    param_grid=param_grid,  # geteste Parameterkombination
+    cv=5,  # Anzahl Folds Cross-Validation, 5 als optimaler Wert
+    scoring='accuracy',  # Bewertungsmetrik
+    verbose=1  # Anzeige Fortschritt & Statuts während Suche, 1 = einfache Ausgabe
 )
 
 # ------------------ Training ------------------
 
-grid_search.fit(X_train, y_train)
+grid_search.fit(X_train, y_train)  # Training
 
 # ------------------ Modell speichern ------------------
 
-best_model = grid_search.best_estimator_
-filename = "model.joblib"
-joblib.dump(best_model, filename)
-
-# ------------------ Modell laden (optional) ------------------
-
-# grid_search = joblib.load("model.joblib")
-# predictions = best_model.predict(X_test)
+best_model = grid_search.best_estimator_    # speichert Model mit besten Paramtern aus GridSearch
+filename = "model.joblib"  # Dateiname für Speicherung
+joblib.dump(best_model, filename)  # Serialisierung Model 
 
 # ------------------ Einstiegspunkt ------------------
 
